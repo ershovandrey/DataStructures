79e469a08588fe8aae829027974a069c044cee52
Andrey Ershov <ershov.andrey@gmail.com>
2020-05-20 14:31:18 +0300

    Fix selection sort

--- algo/sort/selection.js
+++ algo/sort/selection.js
@@ -2,16 +2,20 @@ module.exports = function selectionSort(arr) {
   const length = arr.length;
   for (let i = 0; i < length; i++) {
     let min = i;
-    let temp = arr[i];
     for (let j = i + 1; j < length; j++) {
       if (arr[min] > arr[j]) {
         min = j;
       }
     }
     if (i !== min) {
-      arr[i] = arr[min];
-      arr[min] = temp;
+      swap(arr, i, min);
     }
   }
   return arr;
+}
+
+function swap(arr, i, j) {
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
 }
